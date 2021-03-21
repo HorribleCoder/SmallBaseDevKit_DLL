67414eb895b0477f5962bb9a2ac0786c82be560b
Vladimir Gurenev <vgurenev@gmail.com>
2021-03-21 18:15:30 +0300

    USH Update: Добавил состояние по умолчанию - TimeState, которое позволяет создавть игровые таймеры для Unit.

---
+++ SmallBaseDevKit/USH/Handler/Default/TimerHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using SmallBaseDevKit.USH.State;
+
+namespace SmallBaseDevKit.USH.Handler
+{
+    internal sealed class TimerHandler : BaseGameHandler
+    {
+        public override void ExecuteHandlerLogic(IState currentState)
+        {
+            currentState.ConvertTo<TimerState>().Deconstruct(out var stateParam);
+            if(stateParam.time <= Time.time)
+            {
+                stateParam.timerCallback?.Invoke();
+            }
+        }
+
+        protected override void SetupHandlerOnCreate()
+        {
+
+        }
+    }
+}
---
+++ SmallBaseDevKit/USH/State/Default/TimerState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+
+using SmallBaseDevKit.USH.Handler;
+
+namespace SmallBaseDevKit.USH.State
+{
+    [RequiredHandler(typeof(TimerHandler))]
+    public abstract class TimerState : BaseUnitState<(float time, Action timerCallback)>
+    {
+        private float _time;
+
+        public override void Deconstruct(out (float time, Action timerCallback) stateParam)
+        {
+            stateParam.time = _time;
+            stateParam.timerCallback = TimeCallback;
+        }
+
+        protected override void ExtendedSetupState()
+        {
+            _time = Time.time + SetTimer();
+        }
+
+        protected abstract float SetTimer();
+        protected abstract void TimeCallback();
+    }
+}
