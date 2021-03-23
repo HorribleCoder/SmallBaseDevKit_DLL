d19a3a232fe17101204c955060a05c4a041be6e5
Vladimir Gurenev <vgurenev@gmail.com>
2021-03-23 18:57:36 +0300

    Ref: GameUpdateHandler пеервел в подписку на event методы обновления.

--- SmallBaseDevKit/Main/GameUpdate/GameUpdateHandler.cs
+++ SmallBaseDevKit/Main/GameUpdate/GameUpdateHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 using UnityEngine;
@@ -9,32 +10,32 @@ namespace SmallBaseDevKit.Main
 {
     public sealed class GameUpdateHandler : MonoSingleton<GameUpdateHandler>, IRegistrator<IUpdatable>
     {
-        private LinkedList<IUpdatable> _updatableList;
-        private IList<IUpdatable> _removableList;
+        private event Action OnUpdataeEvent;
+        private event Action OnFixedUpdateEvent;
 
         internal delegate void InvokeLateUpdateSubprocess();
         internal event InvokeLateUpdateSubprocess invokeLateUpdateSubprocessEvent;
 
         public void Registration(IUpdatable obj)
         {
-            if(_updatableList is null)
+            if(obj is IUnit)
             {
-                _updatableList = new LinkedList<IUpdatable>();
+                var c_obj = obj as IUnit;
+                OnUpdataeEvent += c_obj.UpdateUnitState;
             }
-            if (!_updatableList.Contains(obj))
-            {
-                _updatableList.AddLast(obj);
-            }
+            OnUpdataeEvent += obj.OnUpdate;
+            OnFixedUpdateEvent += obj.OnFixedUpdate;
         }
 
         public void Unregistration(IUpdatable obj)
         {
-            if (_updatableList is null) return;
-            if(_removableList is null)
+            if (obj is IUnit)
             {
-                _removableList = new List<IUpdatable>();
+                var c_obj = obj as IUnit;
+                OnUpdataeEvent -= c_obj.UpdateUnitState;
             }
-            _removableList.Add(obj);
+            OnUpdataeEvent -= obj.OnUpdate;
+            OnFixedUpdateEvent -= obj.OnFixedUpdate;
         }
 
         private void Start()
@@ -44,39 +45,12 @@ namespace SmallBaseDevKit.Main
 
         private void Update()
         {
-            //Производим обновление игровых единиц + нвсегда обновляем актуальные состояния и компоненты в игровой единице.
-            var node = _updatableList.First;
-            IUnit unitNode = default;
-            while(node != null)
-            {
-                if (node.Value is IUnit)
-                {
-                    unitNode = (IUnit)node.Value;
-                    unitNode.UpdateUnitState();
-                }
-                node.Value.OnUpdate();
-                node = node.Next;
-            }
+            OnUpdataeEvent?.Invoke();
         }
 
         private void FixedUpdate()
         {
-            var node = _updatableList.First;
-            for (int i = 0; i < _updatableList.Count; ++i)
-            {
-                node.Value.OnFixedUpdate();
-                node = node.Next;
-            }
-        }
-
-        private void LateUpdate()
-        {
-            if (_removableList is null || _removableList.Count == 0) return;
-            for(int i = 0; i < _removableList.Count; ++i)
-            {
-                _updatableList.Remove(_removableList[i]);
-            }
-            _removableList.Clear();
+            OnFixedUpdateEvent?.Invoke();
         }
 
         private IEnumerator LateAddStateForUnitSubprocess()
